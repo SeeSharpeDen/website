d0ecffcb5a2c16a4f2552520c71fe9e92197774c
45ninjas <tom.parker1235@gmail.com>
2022-09-10 23:19:37 +0800

    Fixes #4

--- assets/javascript/loadtime.js
+++ assets/javascript/loadtime.js
@@ -2,5 +2,11 @@ window.onload = function() {
     var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
     console.log('Page load time is ' + loadTime / 1000);
     performanceDisplay = document.getElementById("performance-display") // get a reference to the paragraph
-    performanceDisplay.innerText = loadTime / 1000 // put the value of the variable loadTime into the paragraph
+
+    if(loadTime < 1000)
+    {
+        performanceDisplay.innerText = `${loadTime } ms`; // put the value of the variable loadTime into the paragraph
+    } else {
+        performanceDisplay.innerText = `${loadTime / 1000} seconds`; // put the value of the variable loadTime into the paragraph`
+    }
 }
